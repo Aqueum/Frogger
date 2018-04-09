b2030efb24a5c249b581442753a0a3a5f0373aab
Aqueum <martin.currie@aqueum.com>
2018-04-09 18:23:47 +0100

    fudge: Copy Enemy prototypes to Player

--- js/app.js
+++ js/app.js
@@ -25,11 +25,21 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function() {
+var Player = function(x, y) {
   this.sprite = 'images/char-cat.girl.png';
 };
-Player.prototype = Object.create(Enemy.prototype);
-Player.prototype.constructor = Player;
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
