d88ecfb25ac40834f0752da11937a43bd84173a0
Aqueum <martin.currie@aqueum.com>
2018-04-09 22:55:21 +0100

    docs: Remove duplicate comments

--- js/app.js
+++ js/app.js
@@ -43,13 +43,8 @@ var Player = function(x, y) {
 };
 Player.prototype = Object.create(Actor.prototype);
 Player.prototype.constructor = Player;
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Player.prototype.update = function(dt) {
-  // You should multiply any movement by the dt parameter
-  // which will ensure the game runs at the same speed for
-  // all computers.
-};
+
+Player.prototype.update = function(dt) {};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
