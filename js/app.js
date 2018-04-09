97cacd065b51f2e59883a5571611c58b60a91473
Aqueum <martin.currie@aqueum.com>
2018-04-09 22:12:26 +0100

    refactor: With superclass Actor

--- js/app.js
+++ js/app.js
@@ -1,12 +1,24 @@
+var Actor = function(x, y) {
+  this.x = x;
+  this.y = y;
+};
+Actor.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
   // Variables applied to each of our instances go here,
   // we've provided one for you to get started
 
   // The image/sprite for our enemies, this uses
   // a helper we've provided to easily load images
+  Actor.call(this, x, y);
   this.sprite = 'images/enemy-bug.png';
 };
+// Draw the enemy on the screen, required method for game
+Enemy.prototype = Object.create(Actor.prototype);
+Enemy.prototype.constructor = Enemy;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -16,18 +28,16 @@ Enemy.prototype.update = function(dt) {
   // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
 var Player = function(x, y) {
-  this.sprite = 'images/char-cat.girl.png';
+  Actor.call(this, x, y);
+  this.sprite = 'images/char-cat-girl.png';
 };
+Player.prototype = Object.create(Actor.prototype);
+Player.prototype.constructor = Player;
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Player.prototype.update = function(dt) {
@@ -36,22 +46,17 @@ Player.prototype.update = function(dt) {
   // all computers.
 };
 
-// Draw the enemy on the screen, required method for game
-Player.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
 var allEnemies = [
-  (amy = new Enemy()),
-  (arthur = new Enemy()),
-  (ansel = new Enemy())
+  (amy = new Enemy(0, 75)),
+  (arthur = new Enemy(0, 150)),
+  (ansel = new Enemy(0, 225))
 ];
 
-var player = new Player();
+var player = new Player(300, 300);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
